15bd6f30f9936248501f58692d7475bd53593641
alessiofontanarossa <alessio.font7@gmail.com>
2025-11-20 21:14:40 +0000

    test_main.py improved

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,5 @@
+import os, csv
+from unittest.mock import patch # to leave the original sentiment.csv clean from the test productions
 from fastapi.testclient import TestClient # to simulate a post
 from src.main import app
 
@@ -8,8 +10,14 @@ def test_root():
     assert response.json() == {"message": "Welcome to the Machine Innovators Main Page."}
 
 def test_get_predictions():
+    TEST_CSV_PATH = "data/test_sentiment.csv"
     test_sentences = {"sentences": ["I hate this", "I am neutral.", "I love this."]}
     expected_output = [0, 1, 2] 
-    response = client.post("/inference", json = test_sentences)
-    assert response.status_code == 200
-    assert response.json()['predictions'] == expected_output
+    with patch("src.main.CSV_PATH", TEST_CSV_PATH):
+        response = client.post("/inference", json=test_sentences)
+        assert response.status_code == 200
+        assert response.json()['predictions'] == expected_output
+        assert os.path.exists(TEST_CSV_PATH)
+  
+    if os.path.exists(TEST_CSV_PATH): # clean the test file
+        os.remove(TEST_CSV_PATH)
