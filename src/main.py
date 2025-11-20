f630342f56d197a5d3e75204dbcf1421c9f4cf6a
alessiofontanarossa <alessio.font7@gmail.com>
2025-11-20 19:42:17 +0000

    csv writing added to main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os, csv
 import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -10,15 +11,31 @@ class Reviews(BaseModel):
 
 pipe = get_pipeline() # instantiation just one time
 label_to_int = {'negative': 0, 'neutral': 1, 'positive': 2}
+int_to_label = {i:l for l,i in label_to_int.items()}
 conversion = np.vectorize(label_to_int.get)
+
+if not os.path.exists('data'):
+    os.makedirs('data')
+
+CSV_PATH = 'data/sentiment.csv'
+
+if not os.path.exists(CSV_PATH):
+    with open(CSV_PATH, mode = 'w', newline = '') as file: # writing mode
+        writer = csv.writer(file)
+        writer.writerow(["sentence", "sentiment"])
 
 @app.get("/")
 async def root(): 
-    return {"message": "Machine Innovators Main Page"}
+    return {"message": "Welcome to the Machine Innovators Main Page."}
 
 @app.post("/inference")
 async def get_predictions(new_sentences: Reviews): 
     predictions = get_label_predictions(pipe, conversion, new_sentences.sentences)
+    with open(CSV_PATH, mode = 'a', newline = '') as file: # append mode
+        writer = csv.writer(file)
+        for sentence, sentiment in zip(new_sentences.sentences, predictions):
+            writer.writerow([sentence, int_to_label[int(sentiment)]])
+
     return {"predictions": predictions.tolist()}
 
 if __name__ == "__main__": 
