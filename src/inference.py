3bf8b4cba68092aa1b1235cca452a545f79abb2e
alessiofontanarossa <alessio.font7@gmail.com>
2025-11-21 10:48:00 +0000

    tested container

--- src/inference.py
+++ src/inference.py
@@ -1,10 +1,11 @@
-# this file is used to manually test the API
+# this file is used to manually test the API or the container
 
 import requests
 
-url = "http://127.0.0.1:8000/inference"
+url_api = "http://127.0.0.1:8000/inference"
+url_container = "http://localhost:7500/inference"
 new_data = {"sentences": ["I am very sad", "I am beautiful", "I am neutral"]}
-response = requests.post(url, json = new_data)
+response = requests.post(url_container, json = new_data)
 
 if response.status_code == 200:
     print(response.json()) 
